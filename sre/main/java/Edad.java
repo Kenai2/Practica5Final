c947d481cd44c66153856a8aa4826158a75b0cb7
Kenai2 <kenrivmul@alu.edu.gva.es>
2025-11-12 10:30:41 +0100

    creacion de Proyecto java

---
+++ sre/main/java/Edad.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import java.time.LocalDateTime;
+
+    public class Edad {
+        public static void main(String[] args){
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Introduce tu año de nacimiento:");
+            int anyo = sc.nextInt();
+            LocalDateTime fechaActual = LocalDateTime.now();
+            int edad = fechaActual.getYear() + anyo;
+            if (edad > 18) {
+                System.out.println("Eres mayor de edad");
+            } else {
+                System.out.println("Eres menor de edad");
+            }
+        }
+    }
